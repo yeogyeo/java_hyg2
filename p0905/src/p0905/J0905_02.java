9a628044866cb0029d879a6d97c765e362ca6d81
KOREAVC <KOREAVC@DESKTOP-588IM88>
2023-09-05 10:57:07 +0900

    9월 5일 for if

---
+++ p0905/src/p0905/J0905_02.java
@@ -0,0 +1,24 @@
+package p0905;
+
+public class J0905_02 {
+
+	public static void main(String[] args) {
+		//1부터 10까지의 순번과 합을 출력하시오.
+		int i = 0;//순번
+		int sum = 0; //합계
+		
+		// 위에 int를 선언했으므로 for 괄호안에 int 안써도 됨
+		for(i=1;i<=100;i++) { 
+            sum = sum + i;
+//            System.out.printf("순번 : %d, ",i);
+//            System.out.println("합계 : "+sum);
+            if(sum>=200) {
+            	break;
+            }
+	 }
+		
+		System.out.printf("정답 : %d, %d \n",i,sum);
+		System.out.printf("이전단계 : %d, %d \n",i-1,sum-i);
+
+   }
+}
